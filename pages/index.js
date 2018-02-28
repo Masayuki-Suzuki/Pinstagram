288dba776275f8197f044a94ade48cabe5f5d756
Masayuki Suzuki <m.suzuki.fp@gmail.com>
2018-02-28 15:13:07 -0800

    Set up server with Koa 2.

--- pages/index.js
+++ pages/index.js
@@ -5,45 +5,16 @@ import { changeString, Foo } from "../actions/createActions"
 
 import stylesheet from '../assets/sass/index.scss'
 
-const sample = {
-  display: 'flex',
-  alignItems: 'center',
-  flexDirection: 'column',
-  justifyContent: 'center',
-  height: '100vh',
-  width: '100%',
-}
-
-const sampleHeading = {
-  fontSize: '3rem',
-  lineHeight: 1,
-  margin: '0 0 10px',
-}
-
-const sampleCopy = {
-  fontSize: '2rem',
-  margin: '0 0 40px',
-}
-
-const sampleSubHeading = {
-  fontSize: '1.8rem',
-  fontWeight: 500
-}
-
-const sampleText = {
-  fontSize: '1.6rem'
-}
-
-
 let Page = props => (
-  <div style={sample}>
+  <div className={"sample"}>
     <style dangerouslySetInnerHTML={{ __html: stylesheet }}/>
-    <h1 style={sampleHeading}>Next.js Boilerplate with Redux.</h1>
-    <p className={"commonText"}>This boilerplate is created by Masayuki.</p>
-    <h2 style={sampleSubHeading}>Redux Test ( Input text and push submit )</h2>
+    <h1 className={"sample__heading"}>Next.js Boilerplate with Redux.</h1>
+    <p className={"sample__copy"}>This boilerplate is created by Masayuki.</p>
+    <h2 className={"sample__subHeading"}>Redux Test ( Input text and push submit )</h2>
     <div>
       <input className={"textBox commonText"} type="text" />
       <button
+        className={"sample__btn"}
         onClick={() => {
           props.onSubmit();
         }}
---
+++ server/server.js
@@ -0,0 +1,27 @@
+const Koa     = require('koa'),
+      static   = require('koa-static'),
+      Router  = require('koa-router'),
+      next    = require('next');
+
+const port    = parseInt(process.env.PORT, 10) || 3000,
+      dev     = process.env.NODE_DEV !== 'production',
+      app     = next({dev}),
+      handle  = app.getRequestHandler()
+
+app.prepare()
+  .then( () => {
+    const server = new Koa(),
+          router = new Router()
+
+    router.get('*', async (ctx) => {
+      await handle(ctx.req, ctx.res)
+      ctx.respond = false
+    })
+
+    server.use(static('./public'))
+    server.use(router.routes())
+    server.listen(port, err => {
+      if(err) throw err
+      console.log(`Koa > Ready on http://localhost:${port}`)
+    })
+  })
