37a3066da67b68de9c04b4c19a28bfb5fa5eeecc
Masayuki Suzuki <m.suzuki.fp@gmail.com>
2018-04-07 12:19:33 -0700

    [Delete] Unnecessary function to render component

--- components/loginForm.js
+++ components/loginForm.js
@@ -1,29 +1,5 @@
 import React from 'react'
 
-const RenderLoginForm = ({ isLogin }) => {
-  return (
-    <div>
-      {!isLogin ? null : (
-        <div className="loginForm">
-          <div className="loginForm__logo">
-            <img src="/img/pinstagram-icon.svg" alt="Pinstagram" />
-          </div>
-          <h2 className="loginForm__heading">Login to see and share photos</h2>
-          <form className="login">
-            <label htmlFor="login__email">
-              <input id="login__email" type="email" placeholder="Email" />
-            </label>
-            <label htmlFor="login__">
-              <input id="login__email" type="email" placeholder="Create password" />
-            </label>
-            <button>Log in</button>
-          </form>
-        </div>
-      )}
-    </div>
-  )
-}
-
 const LoginForm = ({ formControl }) => {
   if (formControl.isLogIn) {
     return (
