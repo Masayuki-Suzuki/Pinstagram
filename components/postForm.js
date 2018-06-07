10611d6b76401814e3f2f096c86ee54da4247800
Masayuki Suzuki <m.suzuki.fp@gmail.com>
2018-06-07 16:30:52 -0700

    [Add] Style for post form.

--- components/postForm.js
+++ components/postForm.js
@@ -6,18 +6,27 @@ export default props => (
     <h2 className="postForm__title">Post new Photo</h2>
     <form action="" className="postForm__main">
       <div className="postForm__wrap">
-        <button className="postForm__preview">click to upload from your device.</button>
+        <button className="postForm__preview">
+          <span className="cameraIcon" />
+          Click to upload from your device.
+        </button>
         <div className="postForm__inputGroup">
-          <label htmlFor="postUrl" className="postForm__url">
+          <label htmlFor="postTitle" className="postForm__elm">
+            Title
+            <input type="text" id="postTitle" placeholder="Add the title for this photo." />
+          </label>
+          <label htmlFor="postUrl" className="postForm__elm postForm__url">
+            From website
             <input id="postUrl" type="url" value="" placeholder="Add the URL" />
           </label>
-          <label htmlFor="postDesc" className="postForm__desc">
-            <textarea id="postDesc" className="postForm__textArea" placeholder="" />
-          </label>
-          <label htmlFor="postComment" className="postForm__comment">
-            <textarea id="postComment" className="postForm__textArea" />
+          <label htmlFor="postDesc" className="postForm__elm postForm__desc">
+            Description
+            <textarea id="postDesc" className="postForm__textArea" placeholder="Say more about this photo. *Showing in explorer" rows="5" />
           </label>
         </div>
+      </div>
+      <div className="postForm__submit">
+        <button className="cmnBtn cmnBtn--dbl">submit</button>
       </div>
     </form>
   </FormOverlay>
